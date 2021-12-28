1518f6813380686989f69012e11ee041ef21c1f7
Siddha-code <80904403+Siddha-code@users.noreply.github.com>
2021-12-28 15:30:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,74 @@
+song = "";
+
+function preload()
+{
+	song = loadSound("music.mp3");
+}
+scoreRightWrist = 0;
+scoreLeftWrist = 0;
+rightWristX = 0;
+rightWristY = 0;
+leftWristX = 0;
+leftWristY = 0;
+function setup() {
+    canvas = createCanvas(500,500);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded() {
+    console.log("Kise ko koi fark nihi parta");
+}
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist + "scoreRightWrist = " + scoreRightWrist);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("left_wristx =" + leftWristX+ "left_wrisy =" + leftWristY);
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("right_wristx =" + rightWristX+ "right_wrisy =" + rightWristY);
+    }
+}
+function draw() {
+    image(video,0,0,500,500);
+    fill("red");
+    stroke("red");
+    if (scoreLeftWrist > 0.2) {
+    circle(leftWristX,leftWristY,20);
+    inNumberleftWristY = Number(leftWristY);
+    remove_desimals = floor(inNumberleftWristY);
+    volume = remove_desimals/500;
+    document.getElementById("volume").innerHTML = "volume = " + volume;
+    song.setVolume(volume);
+    }
+    if (scoreRightWrist > 0.2) {
+    circle(rightWristX, rightWristY,20);
+    if(rightWristY > 0 && rightWristY < 101){
+        document.getElementById("speed").innerHTML = "speed = 0.5x";
+        song.rate(0.5);
+    }else if(rightWristY > 100 && rightWristY < 201){
+        document.getElementById("speed").innerHTML = "speed = 1x";
+        song.rate(1);
+    }else if(rightWristY > 200 && rightWristY < 301){
+        document.getElementById("speed").innerHTML = "speed = 1.5x";
+        song.rate(1.5);
+    }else if(rightWristY > 300 && rightWristY < 401){
+        document.getElementById("speed").innerHTML = "speed = 2x";
+        song.rate(2);
+    }else if(rightWristY > 400 && rightWristY < 501){
+        document.getElementById("speed").innerHTML = "speed = 2.5x";
+        song.rate(2.5);
+    }
+}
+}
+function ma_pagal_hu() {
+    song.play();
+    song.setVolume(0.5);
+    song.rate(1);
+}
